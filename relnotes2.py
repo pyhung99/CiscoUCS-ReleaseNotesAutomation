bf474572bc44b766267de5547245d7336f170249
Perry Hung -X (perhung - TRILYON INC at Cisco) <perhung@cisco.com>
2017-02-23 17:02:09 -0800

    iterates over each cell

--- relnotes2.py
+++ relnotes2.py
@@ -10,6 +10,21 @@
 #         array.append(line)
 #         print (array)
 #         print ("\n" + "\n")
+def find_fba(k):
+    delimiter = ' '
+    fba = k.split(delimiter)
+    if len(k) > 8:
+        # print ('Length is great than 8 and so this internal build number must be discarded.')
+        fba = sorted(fba)
+        print (fba[0])
+        x = fba[0]
+        # print ('=== String Length ===') 
+        # print (len(i[2]))
+        return x
+    # else:
+    #     print ('This build value is ok.')
+        # return x
+x = []
 array = []
 temp = []
 readin = open('test1.csv', 'r')
@@ -26,9 +41,12 @@ for i in array:
     print ('Defect:      ' + i[0])
     print ('Description: ' + i[1])
     print ('FBA:         ' + i[2])
-    # print ('              String Length==') 
-    # print (len(i[2]))
     print ('Resolved in: 3.1(2f)')
+    # find_fba(i[2])
+    # i[2] = x
+    # print (x)
+    # print ('=== String Length ===') 
+    # print (len(i[2]))
     # for j in i:
     #     print ("the length of array j is")
     #     print (len(j))
@@ -41,3 +59,4 @@ for i in array:
         # print ('Fixed in: 3.1(2f)')
     # print (i)
     # print ("\n")
+
